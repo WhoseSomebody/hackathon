494acee315dfb494c8c9d026e600fc3768ba275d
SerG-Y <serg.yaschuk@gmail.com>
2017-03-04 18:11:52 +0200

    added api to get video

--- server/routes/api.js
+++ server/routes/api.js
@@ -25,11 +25,15 @@ router.get('/categories', (req, res) => {
             },
             {
                 id: 3,
-                name: "Спорт"
+                name: "Сім'я"
             },
             {
                 id: 4,
-                name: "Мультфільми"
+                name: "Дітям"
+            },
+            {
+                id: 5,
+                name: "Шоу"
             }
         ]
     })
@@ -37,6 +41,49 @@ router.get('/categories', (req, res) => {
 
 
 router.get('categories/:id', (req, res) => {
-
+    res.json({
+        data: [
+            {
+                idSubCategory: "45",
+                title: "Міняю жінку",
+                playlist_subtitle: "реаліті-шоу",
+                image: "https://images.ovva.tv/media/images/e13/b30/53f/e13b3053f4b4c9a41a956b267bd6b9ee.jpeg"
+            },
+            {
+                idSubCategory: "46",
+                playlist_title: "Панянка-селянка",
+                playlist_subtitle: "Реаліті-експеримент",
+                image: "https://images.ovva.tv/media/images/806/b72/460/806b72460bad749053733b7d79cdd3da.jpeg"
+            }
+            ],
+    })
 });
+
+router.get('categories/:id/:idSubCategory', (req, res) => {
+    res.json({
+        data: [
+            {
+                id: "45",
+                title: "Міняю жінку",
+                playlist_subtitle: "реаліті-шоу",
+                image: "https://images.ovva.tv/media/images/e13/b30/53f/e13b3053f4b4c9a41a956b267bd6b9ee.jpeg"
+            },
+            {
+                id: "46",
+                playlist_title: "Панянка-селянка",
+                playlist_subtitle: "Реаліті-експеримент",
+                image: "https://images.ovva.tv/media/images/806/b72/460/806b72460bad749053733b7d79cdd3da.jpeg"
+            }
+        ],
+    })
+});
+
+router.get('/video/:id', (req, res) => {
+    res.json({
+        data: {
+            id: "68MVqpM_gw0"
+        }
+    })
+});
+
 module.exports = router;
