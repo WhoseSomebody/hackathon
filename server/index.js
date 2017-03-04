9b11c69b2f49f6a58ef54f80af96383417a59ead
SerG-Y <serg.yaschuk@gmail.com>
2017-03-04 17:26:23 +0200

    added API

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
+const apiRouter = require('./routes/api');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
+app.use('/api', apiRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ server/routes/api.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const request = require('request');
+
+router.get('/projects', (req, res) => {
+    const lang = "ua";
+    const reqURL = `https://api.ovva.tv/v2/${lang}/playlist`;
+    request(reqURL, (error, response, body) => {
+        if(error || response.statusCode != 200)
+            res.json(error);
+        else
+            res.json({data: JSON.parse(body)})
+    })
+});
+
+router.get('/categories', (req, res) => {
+    res.json({
+        data: [
+            {
+                id: 1,
+                name: "Новини"
+            },
+            {
+                id: 2,
+                name: "Розваги"
+            },
+            {
+                id: 3,
+                name: "Спорт"
+            },
+            {
+                id: 4,
+                name: "Мультфільми"
+            }
+        ]
+    })
+});
+
+
+router.get('categories/:id', (req, res) => {
+});
+module.exports = router;
