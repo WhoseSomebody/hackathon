1898c1a2704091b4955414034b39271b8bcb78fd
Yaroslav Oshyyko <yaroslav.oshiyko.5@gmail.com>
2017-03-04 13:37:33 +0200

    Added client starter

---
+++ client/README.md
@@ -0,0 +1,18 @@
+# Int20h Starter
+
+## Setup
+
+```
+# clone the repo
+git clone https://github.com/yaroslavOshyyko/int20h-starter.git
+
+# install dependencies
+yarn
+# or 
+npm install
+
+# start the server
+npm start
+```
+
+Server runs in localhost:3000.
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+@Component({
+  selector: 'home',  // <home></home>
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+  
+  constructor() {}
+
+  public ngOnInit() {}
+}
