ccc5394cac1e5290a16aab803c21152289f28174
SerG-Y <serg.yaschuk@gmail.com>
2017-03-04 22:32:30 +0200

    Added CORS

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const apiRouter = require('./routes/api');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use('/', routes);
 app.use('/api', apiRouter);
