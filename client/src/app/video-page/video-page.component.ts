2c1c50ca2882df3411c784b8f916a5cce0be3141
WhoseSomebody <whosesomebody@gmail.com>
2017-03-05 05:10:26 +0200

    Video player. des 1

--- client/src/app/video-page/video-page.component.ts
+++ client/src/app/video-page/video-page.component.ts
@@ -2,14 +2,15 @@ import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'video-page',
-    templateUrl: 'video-page.component.html'
+    templateUrl: 'video-page.component.pug',
+    styleUrls: ['video-page.component.styl']
 })
 export class VideoPageComponent implements OnInit {
-    
+
     private videoId: string;
     constructor(private route: ActivatedRoute) { }
 
-    ngOnInit() { 
+    ngOnInit() {
         this.route.params.subscribe(params => {
             this.videoId = params.id;
         });
--- client/src/app/video-page/video-player/video-player.component.ts
+++ client/src/app/video-page/video-player/video-player.component.ts
@@ -4,27 +4,27 @@ import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
     selector: 'video-player',
-    templateUrl: 'video-player.component.html',
+    templateUrl: 'video-player.component.pug',
     styleUrls: ['video-player.component.styl']
 })
 export class VideoPlayerComponent implements OnInit {
     player;
     @Input() public videoId: string;
-    
+
     constructor(private route: ActivatedRoute){
 
     }
 
     ngOnInit() {
-        
+
     }
 
     savePlayer (player) {
         this.player = player;
-        player.seekTo(10, false);
+        player.stopVideo();
         console.log('player instance', player)
     }
-    
+
     onStateChange(event){
         console.log('player state', event.data);
     }
